1325f59834e5962ef4b2937d67ef0d6270cc3c39
nbomanji-r7 <navroze_bomanji@rapid7.com>
2018-02-06 01:42:22 +0530

    Alpha Beta Pruning

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -122,7 +122,7 @@ int maxSearch(char board[][3])
         {
             if (board[i][j] == space)
             {
-                board[i][j] = 'x';
+                board[i][j] = player;
                 score = max(score, minSearch(board));
                 board[i][j] = space;
             }
@@ -132,84 +132,158 @@ int maxSearch(char board[][3])
     return score;
 }
 
-// This will return the best possible move for the player
-Move minimax(char board[][3]) 
+// This will return the best possible move for the ai
+int minimax(char board[][3],bool isMaxPlayer ,int alpha, int beta) 
 {
-    int score = numeric_limits<int>::max();
-    Move move;
+	if (checkWin(board, player)) { return -10; }
+        else if (checkWin(board, ai)) { return +10; }
+        else if (checkTie(board)) { return 0; }
 
-    for (int i = 0; i < 3; i++)
+    if(isMaxPlayer)
     {
-        for (int j = 0; j < 3; j++)
+    	int bestVal = INT_MIN;
+    	for ( int i = 0; i < 3; i++)
+    	{
+    	    for ( int j = 0; j < 3; j++)
+    	    {
+    	        if (board[i][j] == space)
+    	        {
+    	            board[i][j] = player;
+    	            bestVal = max(bestVal, minimax(board, false, alpha, beta));
+    	            board[i][j] = space;
+    	            alpha = max(alpha, bestVal);
+    	            if(alpha >= beta)
+    	            	break;
+    	        }
+    	    }
+    	}
+    }
+    else  
+    {
+		int bestVal = INT_MAX;
+		for ( int i = 0; i < 3; i++)
+		{
+		    for ( int j = 0; j < 3; j++)
+		    {
+		        if (board[i][j] == space)
+		        {
+		            board[i][j] = ai;
+		            bestVal = min(bestVal, minimax(board, true, alpha, beta));
+		            board[i][j] = space;
+		            alpha = min(alpha, bestVal);
+		            if(alpha >= beta)
+		            {
+		            	break;
+		            }
+		        }
+		    }
+		}
+		return bestVal;
+    }
+}
+
+// This will return the best possible move for the player
+Move findBestMove(char board[][3])
+{
+    int bestVal = -1000;
+    Move bestMove;
+    bestMove.row = -1;
+    bestMove.col = -1;
+ 
+    // Traverse all cells, evalutae minimax function for
+    // all empty cells. And return the cell with optimal
+    // value.
+    for (int i = 0; i<3; i++)
+    {
+        for (int j = 0; j<3; j++)
         {
-            if (board[i][j] == space)
+            // Check if celll is empty
+            if (board[i][j]==space)
             {
+                // Make the move
                 board[i][j] = ai;
-
-                int temp = maxSearch(board);
-
-                if (temp < score)
-                {
-                    score = temp;
-                    move.row = i;
-                    move.col = j;
-                }
+ 
+                // compute evaluation function for this
+                // move.
+                int moveVal = minimax(board, true, INT_MIN, INT_MAX);
+ 
+                // Undo the move
                 board[i][j] = space;
+ 
+                // If the value of the current move is
+                // more than the best value, then update
+                // best/
+                if (moveVal > bestVal)
+                {
+                    bestMove.row = i;
+                    bestMove.col = j;
+                    bestVal = moveVal;
+                }
             }
         }
     }
 
-    return move;
+    return bestMove;
 }
 
 
 //Start the game
-void startGame(char board[][3])
+void startGame()
 {
-	int row, column, score;
-	while(isMovesLeft(board))
-	{
-		do{
-			cout<<"\nEnter the row[1-3] and column[1-3] | (Example: 2 3) | = ";
-			cin>>row>>column;	
-			cout<<endl;
-		} while((row > 3 || row < 1) || column > 3 || column < 1);
-		board[row-1][column-1] = 'x';
-		displayBoard(board);
-		if(checkWin(board, player))
+	char board[3][3] =
 		{
-			cout<<"--------------"<<endl;
-			cout<<"Player wins!!!"<<endl;
-			cout<<"--------------"<<endl;
-			return;
-		}
-		Move bestMove = minimax(board);
-		board[bestMove.row][bestMove.col] = 'o';
-		cout<<"\nComputer has played: "<<bestMove.row+1<<" "<<bestMove.col+1<<endl<<endl;
-		displayBoard(board);
-		if(checkWin(board, ai))
-		{
-			cout<<"----------------"<<endl;
-			cout<<"Computer wins!!!"<<endl;
-			cout<<"----------------"<<endl<<endl;
-			return;	
-		}
-		if (checkTie(board))
-		{
-			cout<<"------------------"<<endl;
-		    cout << "\n*** MATCH DRAW ***\n";
-		    cout<<"------------------"<<endl<<endl;
-		}	
-	}
+			{ 'o', 'o', 'x' },
+			{ '_', 'o', 'x' },
+			{ 'x', '_', '_' }
+		}; 
+	Move bestMove = findBestMove(board);
+
+		printf("ROW: %d COL: %d", bestMove.row,
+									bestMove.col );
+	// int row, column, score;
+	// while(isMovesLeft(board))
+	// {
+	// 	do{
+	// 		cout<<"\nEnter the row[1-3] and column[1-3] | (Example: 2 3) | = ";
+	// 		cin>>row>>column;	
+	// 		cout<<endl;
+	// 	} while((row > 3 || row < 1) || column > 3 || column < 1);
+	// 	board[row-1][column-1] = 'x';
+	// 	displayBoard(board);
+	// 	if(checkWin(board, player))
+	// 	{
+	// 		cout<<"--------------"<<endl;
+	// 		cout<<"Player wins!!!"<<endl;
+	// 		cout<<"--------------"<<endl;
+	// 		return;
+	// 	}
+	// 	Move bestMove = findBestMove(board);
+	// 	board[bestMove.row][bestMove.col] = 'o';
+	// 	cout<<"\nComputer has played: "<<bestMove.row+1<<" "<<bestMove.col+1<<endl<<endl;
+	// 	displayBoard(board);
+	// 	if(checkWin(board, ai))
+	// 	{
+	// 		cout<<"----------------"<<endl;
+	// 		cout<<"Computer wins!!!"<<endl;
+	// 		cout<<"----------------"<<endl<<endl;
+	// 		return;	
+	// 	}
+	// 	if (checkTie(board))
+	// 	{
+	// 		cout<<"------------------"<<endl;
+	// 	    cout << "\n*** MATCH DRAW ***\n";
+	// 	    cout<<"------------------"<<endl<<endl;
+	// 	}	
+	// }
 }
 
 // Driver code
 int main()
 {
-	char board[3][3];
+	// char board[3][3];
 	char choice;
 	do{
-		initBoard(board);
+		// initBoard(board);
 		cout<<"Would you like to play a game of tic-tac-toe: \n1).Y(Yes)\n2).N(No)?\n3).Press ctrl+c to exit\n";
 		cin>>choice;
 		if(choice == 'n' || choice == 'N')
@@ -217,7 +291,7 @@ int main()
 		cout<<"======================="<<endl;
 		cout<<"Welcome to tic-tac-toe!"<<endl;
 		cout<<"======================="<<endl;
-		startGame(board);
+		startGame();
 	} while(choice == 'y' || choice == 'Y');
 	return 0;
 }
